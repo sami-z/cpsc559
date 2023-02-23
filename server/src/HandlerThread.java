77ddebcdfea8b156fbdbdbe79749d5a3d7a107ad
Manbir1 <mana24129@hotmail.com>
2023-02-22 18:34:24 -0700

    stuff

---
+++ server/src/HandlerThread.java
@@ -0,0 +1,10 @@
+import java.net.Socket
+public class HandlerThread implements Runnable {
+    public HandlerThread(Socket clientSocket, String string){
+
+    }
+
+    public void run(){
+
+    }
+}
---
+++ server/src/MultiThreadedServer.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiThreadedServer implements Runnable{
+    int serverPort = 8080;
+    ServerSocket serverSocket = null;
+    boolean isRunning = false;
+
+
+    public MultiThreadedServer(int port) {
+        this.serverPort = port;
+    }
+
+    public synchronized void stop() {
+        this.isRunning = false;
+    }
+
+    private synchronized void openServerSocket() {
+        try {
+            serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Override
+    public void run() {
+        this.isRunning = true;
+        this.openServerSocket();
+
+        while(!this.isRunning){
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if(this.isRunning) {
+                    System.out.println("Server Stopped.") ;
+                    return;
+                }
+                throw new RuntimeException(
+                        "Error accepting client connection", e);
+            }
+            new Thread(
+                    new HandlerThread(
+                            clientSocket, "Multithreaded Server")
+            ).start();
+        }
+        System.out.println("Server Stopped.") ;
+
+        System.out.println("Closing server");
+    }
+}
