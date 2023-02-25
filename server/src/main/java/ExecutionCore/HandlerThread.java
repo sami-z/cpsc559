4147482f44acbf26f1254ecc0dde8393c082b941
austinficzere <60748773+austinficzere@users.noreply.github.com>
2023-02-24 20:17:09 -0700

    added server and response queue connection

--- server/src/main/java/ExecutionCore/HandlerThread.java
+++ server/src/main/java/ExecutionCore/HandlerThread.java
@@ -1,5 +1,6 @@
 package ExecutionCore;
 
+import Kafka.ResponseQueueProducer;
 import Models.ClientRequestModel;
 import Network.HTTPRequestParser;
 import Network.HttpFormatException;
@@ -8,8 +9,10 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import DBServ.DB;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 
@@ -18,6 +21,7 @@ public class HandlerThread implements Runnable {
     private String messageString;
     private Socket clientSocket;
     private DB db;
+    private ResponseQueueProducer producer;
 
     public HandlerThread(String message){
         this.messageString = message;
@@ -29,22 +33,24 @@ public class HandlerThread implements Runnable {
         Gson g = new Gson();
         ClientRequestModel request = g.fromJson(messageString, ClientRequestModel.class);
 
-
-
         // If contains file save file
         if(!request.bytes.isEmpty()) {
 //            Path tempFile = Files.createTempFile(null, null);
 //            Files.write(tempFile, request.bytes.getBytes(StandardCharsets.UTF_8));
-            //
         }
         // Check type of request
         if(request.requestType.toUpperCase().equals("READ")){ // locking
+            ArrayList<JSONObject> files;
             try {
-                db.findFiles(request.userName);
+                files = db.findFiles(request.userName);
             } catch (ParseException e) {
                 throw new RuntimeException(e);
             }
-            // Send to response queue
+
+            for (JSONObject jsonObject : files) {
+                String serializedJson = jsonObject.toString();
+                producer.sendMessages(request.userName, serializedJson);
+            }
         }
         else if(request.requestType.toUpperCase().equals("WRITE")){ // locking
             try {
@@ -52,7 +58,7 @@ public class HandlerThread implements Runnable {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            // Send to response queue
+            producer.sendMessages(request.userName,"SUCCESS");
         }
     }
 }
