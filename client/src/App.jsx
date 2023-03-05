93518c3a22b82852075cca1c6165d4c6e22b22fc
rajode <rgmittal5@gmail.com>
2023-03-05 12:11:39 -0700

    important bug fix
    fixed complex bug with sidebar error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Files from "./components/Files/Files"
-import Sidebar from "./components/Sidebar"
+import Sidebar from "./components/sidebar"
 import { useState, useEffect } from 'react';
 import { RESPONSE_QUEUE_SERVER_PORT } from './components/WebSocket/WebSocket';
 
