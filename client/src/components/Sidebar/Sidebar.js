a4d226cc5f527a9d2aa3613dc48b8a3940f16ec1
rajode <rgmittal5@gmail.com>
2023-03-05 12:48:50 -0700

    added Sidebar.js
    removed index.js

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Upload from './Upload'
+import Share from './Share'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <Upload />
+
+      <Share />
+
+
+
+
+
+      <div className='sidebar_container'>
+
+
+        <hr />
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
