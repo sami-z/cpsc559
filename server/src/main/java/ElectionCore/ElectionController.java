547bb6f75da7f089ba2756d33eb1d071f211c0a0
Manbir1 <mana24129@hotmail.com>
2023-03-04 22:08:04 -0700

    aaaa

--- server/src/main/java/ElectionCore/ElectionController.java
+++ server/src/main/java/ElectionCore/ElectionController.java
@@ -1,18 +1,31 @@
-package ExecutionCore;
+package ElectionCore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class ElectionController{
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @Autowired
+    private ElectionState state;
+
+    @PostMapping("/leader")
+    public String leader(ElectionState es) {
+
+    }
+
+    @PostMapping("/election")
+    public String election(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
 
 }
--- server/src/main/java/ElectionCore/ElectionState.java
+++ server/src/main/java/ElectionCore/ElectionState.java
@@ -1,2 +1,21 @@
-package ElectionCore;public class ElectionState {
+package ElectionCore;
+
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Component
+public class ElectionState {
+    public InetAddress serverIP;
+    public boolean isRunning;
+    public InetAddress leaderIP;
+
+    public ElectionState() throws UnknownHostException {
+        this.isRunning = false;
+        leaderIP = null;
+        serverIP = InetAddress.getLocalHost();
+    }
+
+
 }
