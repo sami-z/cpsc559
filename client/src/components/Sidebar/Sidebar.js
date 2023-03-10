851b788f32e548e198b2356e330fd9f12f8e5aac
sami-z <samizeremariam@gmail.com>
2023-03-09 22:01:52 -0700

    Added delete button and changed websocket address

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Upload from './Upload'
 import Share from './Share'
+import Delete from './Delete'
 import './styles.css'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Upload />
       <Share />
+      <Delete />
     </div>
   )
 }
--- client/src/components/WebSocket/WebSocket.js
+++ client/src/components/WebSocket/WebSocket.js
@@ -12,5 +12,5 @@
 //     console.log('Received message from WebSocket:', event.data);
 //   };
 
-export const WEBSOCKET_URL = "ws://198.53.22.182:80";
-export const RESPONSE_QUEUE_SERVER_PORT = "ws://localhost:8080";
+export const WEBSOCKET_URL = "ws://localhost:80";
+export const RESPONSE_QUEUE_SERVER_PORT = "ws://localhost:9090";
