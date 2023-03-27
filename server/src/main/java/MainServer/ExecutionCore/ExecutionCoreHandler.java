d1c9a1c06881a7fdd3e0e68a8eb048d108eaaedb
Manbir Sandhu <mana24129@hotmail.com>
2023-03-27 00:26:17 -0600

    Changed things to use request type

--- server/src/main/java/MainServer/ExecutionCore/ExecutionCoreHandler.java
+++ server/src/main/java/MainServer/ExecutionCore/ExecutionCoreHandler.java
@@ -58,156 +58,136 @@ public class ExecutionCoreHandler {
 
         if (request == null) return;
 
+        String requestType = request.get("requestType").asText();
 
-        // Check type of request
-        if(request.get("requestType").asText().equalsIgnoreCase("READ")){ // locking
+        if(requestType.equalsIgnoreCase("READ_ALL_FILES")){
             System.out.println("database requestType" + System.currentTimeMillis());
-            String readType = request.get("readType").asText();
+            ArrayList<JsonNode> files = db.findFiles(request.get("userName").asText());
 
-            System.out.println("database requestType" + System.currentTimeMillis());
-            if(readType.equals("allFiles")){
-                ArrayList<JsonNode> files = db.findFiles(request.get("userName").asText());
+            JsonNode response;
 
-                JsonNode response;
+            ObjectMapper objectMapper = new ObjectMapper();
+            if (!files.isEmpty()){
+                response = objectMapper.valueToTree(files);
+            }
+            else{
+                response = objectMapper.createObjectNode();
+                ((ObjectNode)response).put("responseType", "ALLFILESEMPTY");
+                ((ObjectNode)response).put("userName", request.get("userName").asText());
+            }
 
-                ObjectMapper objectMapper = new ObjectMapper();
-                if (!files.isEmpty()){
-                    response = objectMapper.valueToTree(files);
-                }
-                else{
-                    response = objectMapper.createObjectNode();
-                    ((ObjectNode)response).put("readType", "ALLFILESEMPTY");
-                    ((ObjectNode)response).put("userName", request.get("userName").asText());
-                }
-
-                for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
+            for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
 //                    for (JsonNode file : files) {
 //                        System.out.println("the actual file"+file);
 //                        sendToResponseQueue(file, IP);
 //                    }
-                    NetworkUtil.sendToResponseQueue(response, IP);
+                NetworkUtil.sendToResponseQueue(response, IP);
 
 //                    sendToResponseQueue(response, IP);
-                }
-                System.out.println("database blah" + System.currentTimeMillis());
             }
+            System.out.println("database blah" + System.currentTimeMillis());
+        }else if(requestType.equalsIgnoreCase("READ")){
+            System.out.println("DATABASE SINGLE BEFORE" + System.currentTimeMillis());
+            JsonNode singleFile = db.loadFile(request.get("fileName").asText());
+            System.out.println("DATABASE SINGLE AFTER LOAD" + System.currentTimeMillis());
 
-            else if(readType.equals("SINGLE")){
-                System.out.println("DATABASE SINGLE BEFORE" + System.currentTimeMillis());
-                JsonNode singleFile = db.loadFile(request.get("fileName").asText());
-                System.out.println("DATABASE SINGLE AFTER LOAD" + System.currentTimeMillis());
-
-                ((ObjectNode)singleFile).put("readType", "SINGLE");
-                for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
+            ((ObjectNode)singleFile).put("responseType", "SINGLE");
+            for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
 //                    for (JsonNode file : files) {
 //                        System.out.println("the actual file"+file);
 //                        sendToResponseQueue(file, IP);
 //                    }
-                    NetworkUtil.sendToResponseQueue(singleFile, IP);
-                }
-                System.out.println("DATABASE SINGLE FOR LOOP" + System.currentTimeMillis());
-
+                NetworkUtil.sendToResponseQueue(singleFile, IP);
             }
+            System.out.println("DATABASE SINGLE FOR LOOP" + System.currentTimeMillis());
+        } else if(requestType.equalsIgnoreCase("LOGIN")){
+            FindIterable<Document> entry = db.getLoginReplica(true).find(eq("userName", request.get("userName").asText()));
 
-            else if (readType.equals("LOGIN")){
-                FindIterable<Document> entry = db.getLoginReplica(true).find(eq("userName", request.get("userName").asText()));
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode response = mapper.createObjectNode();
+            ((ObjectNode)response).put("userName", request.get("userName").asText());
+            ((ObjectNode)response).put("responseType", "LOGIN");
 
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode response = mapper.createObjectNode();
-                ((ObjectNode)response).put("userName", request.get("userName").asText());
-                ((ObjectNode)response).put("readType", "LOGIN");
+            for (Document doc : entry) {
+                String actualUserName = doc.getString("userName");
+                String actualPassword = doc.getString("password");
 
-                for (Document doc : entry) {
-                    String actualUserName = doc.getString("userName");
-                    String actualPassword = doc.getString("password");
-
-                    if (request.get("userName").asText().equals(actualUserName) && request.get("password").asText().equals(actualPassword)) {
-                        ((ObjectNode) response).put("loggedIn", "SUCCESS");
-                        break; // break the loop once a match is found
-                    } else {
-                        ((ObjectNode) response).put("loggedIn", "FAILURE");
-                    }
-                }
-
-                if (!response.has("loggedIn")) {
-                    // handle case where no match was found
+                if (request.get("userName").asText().equals(actualUserName) && request.get("password").asText().equals(actualPassword)) {
+                    ((ObjectNode) response).put("loggedIn", "SUCCESS");
+                    break; // break the loop once a match is found
+                } else {
                     ((ObjectNode) response).put("loggedIn", "FAILURE");
                 }
-
-                for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
-                    NetworkUtil.sendToResponseQueue(response, IP);
-                }
             }
 
-        }
-        else if(request.get("requestType").asText().equalsIgnoreCase("WRITE")){
-            String writeType = request.get("writeType").asText();
+            if (!response.has("loggedIn")) {
+                // handle case where no match was found
+                ((ObjectNode) response).put("loggedIn", "FAILURE");
+            }
+
+            for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
+                NetworkUtil.sendToResponseQueue(response, IP);
+            }
+        } else if(requestType.equalsIgnoreCase("WRITE")){
             String fileName = request.get("fileName").asText();
             obtainLock(ServerState.requestQueueIP,request);
 
-            if(writeType.equals("DELETE")){
-                String deleteList = NetworkUtil.sendDelete(request);
-                System.out.println("PRINITNG  DELETE LIST: " + deleteList);
+            System.out.println("Send to database" + System.currentTimeMillis());
 
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode response = mapper.createObjectNode();
-                ((ObjectNode)response).put("userName", request.get("userName").asText());
-                ((ObjectNode)response).put("readType", "DELETE");
-                ((ObjectNode)response).put("delete", deleteList);
+            System.out.println("Request is: " + request);
 
 
+            NetworkUtil.sendWrite(request);
 
-                System.out.println("PRINITNG RESPONSE LIST: " + response);
-
-
-                for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
-                    NetworkUtil.sendToResponseQueue(response, IP);
-                }
+            System.out.println("database write done" + System.currentTimeMillis());
+            for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
+                NetworkUtil.sendToResponseQueue(request, IP);
             }
 
-            else if (writeType.equals("SHARE")) {
-                ArrayList<String> arr = new ObjectMapper().convertValue(request.get("sharedWith"), ArrayList.class);
-                db.editSharedWith(fileName, arr);
-            }
-
-            else if (writeType.equals("REGISTER")){
-                boolean wasSuccessful = NetworkUtil.sendRegister(request);
-
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode response = mapper.createObjectNode();
-                ((ObjectNode)response).put("userName", request.get("userName").asText());
-                ((ObjectNode)response).put("writeType", "REGISTER");
-
-                if (wasSuccessful) {
-                    ((ObjectNode) response).put("registered", "SUCCESS");
-                } else {
-                    ((ObjectNode) response).put("registered", "FAILURE");
-                }
-
-                for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
-                    NetworkUtil.sendToResponseQueue(response, IP);
-                }
-            }
-            else {
-
-                System.out.println("Send to database" + System.currentTimeMillis());
-
-                System.out.println("Request is: " + request);
-
-
-                NetworkUtil.sendWrite(request);
-
-                System.out.println("database write done" + System.currentTimeMillis());
-                for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
-                    NetworkUtil.sendToResponseQueue(request, IP);
-                }
-
-                System.out.println("Responsequeue sent" + System.currentTimeMillis());
-
-
-            }
+            System.out.println("Responsequeue sent" + System.currentTimeMillis());
 
             NetworkUtil.releaseLock(ServerState.requestQueueIP,fileName);
+        }
+        else if(requestType.equalsIgnoreCase("SHARE")){
+            String fileName = request.get("fileName").asText();
+            ArrayList<String> arr = new ObjectMapper().convertValue(request.get("sharedWith"), ArrayList.class);
+            db.editSharedWith(fileName, arr);
+        } else if(requestType.equalsIgnoreCase("DELETE")){
+
+            String deleteList = NetworkUtil.sendDelete(request);
+            System.out.println("PRINITNG  DELETE LIST: " + deleteList);
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode response = mapper.createObjectNode();
+            ((ObjectNode)response).put("userName", request.get("userName").asText());
+            ((ObjectNode)response).put("responseType", "DELETE");
+            ((ObjectNode)response).put("delete", deleteList);
+
+
+
+            System.out.println("PRINITNG RESPONSE LIST: " + response);
+
+
+            for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
+                NetworkUtil.sendToResponseQueue(response, IP);
+            }
+        } else if (requestType.equals("REGISTER")){
+            boolean wasSuccessful = NetworkUtil.sendRegister(request);
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode response = mapper.createObjectNode();
+            ((ObjectNode)response).put("userName", request.get("userName").asText());
+            ((ObjectNode)response).put("responseType", "REGISTER");
+
+            if (wasSuccessful) {
+                ((ObjectNode) response).put("registered", "SUCCESS");
+            } else {
+                ((ObjectNode) response).put("registered", "FAILURE");
+            }
+
+            for(String IP : NetworkConstants.RESPONSE_QUEUE_IPS){
+                NetworkUtil.sendToResponseQueue(response, IP);
+            }
         }
         else{
             System.out.println("invalid request type (must be READ or WRITE)");
