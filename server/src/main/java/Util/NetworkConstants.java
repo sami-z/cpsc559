0b134f0ecb030937834e94f94f58c5f047f065b3
Manbir1 <mana24129@hotmail.com>
2023-04-10 12:49:24 -0600

    addded response queue ip

--- server/src/main/java/Util/NetworkConstants.java
+++ server/src/main/java/Util/NetworkConstants.java
@@ -9,7 +9,7 @@ public final class NetworkConstants {
     public static int RESPONSE_QUEUE_PORT = 8080;
     public static int DATABASE_MANAGER_PORT = 8080;
     public static String[] REQUEST_QUEUE_IPS = new String[] {"18.189.196.143","3.136.94.117"};
-    public static String[] RESPONSE_QUEUE_IPS = new String[] {"172.31.7.43","172.31.11.39"};
+    public static String[] RESPONSE_QUEUE_IPS = new String[] {"18.224.111.143","18.216.20.109"};
     public static String[] DB_MANAGER_IP = {"172.31.14.243","172.31.3.16"};
     public static String EMPTY_DB_LEADER = "EMPTY";
     public static int MAX_RETRY = 5;
