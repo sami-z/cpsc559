486d0a2bf32321505a52143fbf50537700470516
Jake Kim <jake.kim103@gmail.com>
2023-04-09 22:03:15 -0600

    Write javadoc for all classes under DatabaseManager package

--- server/src/main/java/DatabaseManager/DBMain.java
+++ server/src/main/java/DatabaseManager/DBMain.java
@@ -8,6 +8,14 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 import java.util.Collections;
 
+/**
+
+ The main class for running the database manager application. This class is annotated with the @SpringBootApplication
+ annotation to enable Spring Boot autoconfiguration. It also excludes the MongoAutoConfiguration and MongoDataAutoConfiguration
+ classes to prevent Spring Boot from automatically configuring MongoDB.
+ The main method sets the default properties for the application, including the server port, and starts the application using
+ the SpringApplication class. The method also initializes the DBManagerState.DBLeaderIP to an empty string.
+ */
 @SpringBootApplication(exclude = {
         MongoAutoConfiguration.class,
         MongoDataAutoConfiguration.class
--- server/src/main/java/DatabaseManager/DataAccessObject/DatabaseTimestampsImpl.java
+++ server/src/main/java/DatabaseManager/DataAccessObject/DatabaseTimestampsImpl.java
@@ -9,12 +9,25 @@ public class DatabaseTimestampsImpl implements DatabaseTimestamps{
 
     private static final HashMap<String, Long> timestampMap = new HashMap<>();
 
+    /**
+
+     Updates the latest timestamp for a given user and file.
+     @param userName the name of the user
+     @param fileName the name of the file
+     @param timestamp the latest timestamp to be updated
+     */
     @Override
     public void updateLatestTimestamp(String userName, String fileName, long timestamp) {
         String key = String.join(",", userName, fileName);
         timestampMap.put(key, timestamp);
     }
 
+    /**
+
+     Returns the latest timestamp for the specified key.
+     @param key the key for which to retrieve the latest timestamp
+     @return the latest timestamp for the specified key, or -1 if the key is not found in the timestamp map
+     */
     @Override
     public long getLatestTimestamp(String key) {
         if (!timestampMap.containsKey(key)) return -1;
